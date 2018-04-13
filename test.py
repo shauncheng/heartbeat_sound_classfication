1fb3909272771ded7966637df52f9547c5ca428e
yonghumingdoumeiyou <azhu@users.noreply.github.com>
2018-04-13 21:43:49 +0800

    生成训练集

--- test.py
+++ test.py
@@ -11,16 +11,13 @@ from pre_process import ToHdf5
 import numpy as np
 def main():
     print('model launch')
-    audioTool=AudioTool('./audio_data/training-a/a0001.wav')
-    segVector=audioTool.getSegment(length=2000,isPlot=True)
-    print('截取长度:2000')
-    
-    tx=np.random.randn(20,2048,1)
-    ty=np.random.randn(20,1)
-    vx=np.random.randn(10,2048)
-    vy=np.random.randn(10)
-    train= ToHdf5()
-    train.writeData('testHdf5.hdf5',tx,ty,vx,vy)
+    audioTool=AudioTool('../test','.wav')
+    train_X=audioTool.cutAudio(2048)
+    train_Y=audioTool.get_csv()
+#    print(train_X[17])
+    print(train_Y[0:17,:])
+#    train= ToHdf5()
+#    train.writeData('testHdf5.hdf5',tx,ty,vx,vy)
 #    a,b,b,d=train.loadData('testHdf5.hdf5')
 #    print(a[1,:,:])
 if __name__=='__main__':
